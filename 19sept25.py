e652b4c9d8ad1da021d50954dfcadb13d10ea170
Upendrakushwah <pratapupendra218@gmail.com>
2025-09-19 09:28:43 +0530

    Update 19sept25.py

--- 19sept25.py
+++ 19sept25.py
@@ -10,4 +10,13 @@ for i in range (rows ,0 ,-1)
     print ( "*", end =" ")
 print()    
 
+rows = int (input("enter the row size for the pattern"))
+for i in range (rows ,0 ,-1):
+    for j in range (rows-1): 
+      print(" ", end =" ")
+    for k in range (1 , 2*i):
+        print ( "*", end = " ")
+    print()    
+    
+
 
