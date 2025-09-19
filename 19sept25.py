ac049b59472f7681b797d6378b9859bcb7893276
Upendrakushwah <pratapupendra218@gmail.com>
2025-09-19 09:18:55 +0530

    Update 19sept25.py

--- 19sept25.py
+++ 19sept25.py
@@ -2,4 +2,12 @@ rows = int (input("enter the row size for the pattern"))
 for i in range (1, rows+1):
     for j in range (1, i+1):
         print ( " * ", end = " ")
-        print()
+print()
+
+rows = int (input("enter the row size for the pattern "))
+for i in range (rows ,0 ,-1)
+     for j in range (1, i+1)
+    print ( "*", end =" ")
+print()    
+
+
